c5f25b48086d60aba540670283ec69d90243c415
DasMoge124 <75040379+DasMoge124@users.noreply.github.com>
2020-12-31 22:10:02 -0800

    Initial commit

---
+++ Gun.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+	public float damage = 10f;
+	public float range = 100f;
+	public float ammo = 50f;
+	
+	public Camera fpsCam;
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && ammo > 0f)
+		{
+			Shoot();
+			ammo --;
+		}
+		if (Input.GetKeyDown(KeyCode.R) && ammo <= 0)
+		{
+			ammo = 50f;
+		}
+    }
+	void Shoot ()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+		{
+			Debug.Log(hit.transform.name);
+			
+			Target target= hit.transform.GetComponent<Target>();
+			if (target != null)
+			{
+				target.TakeDamage(damage);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FPS-PlayerController
+ This is basically an FPS Controller I made. You can use it whenever you want and make changes to it.
